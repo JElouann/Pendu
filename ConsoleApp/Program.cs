9200b688b45e879006d984b21ccbf0eda54de825
JElouann <eljouenne@gmail.com>
2026-03-05 22:54:16 +0100

    sinistre le TP

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿var client = new HttpClient();
+
+var response = await client.GetAsync("https://localhost:17225/");
+
+response = await client.PostAsync("https://localhost:17225/game/new", new StringContent("word"));
+//var body = await response.Content.ReadAsStringAsync();
+
+//Console.WriteLine($"{(int)response.StatusCode}: {response.StatusCode}");
+//Console.WriteLine(body);
+
+//Console.ReadLine();
+
